369b7ec3183dda0b9dd37b7af1932661d3eb7f4f
Amna <ui.amnahanif@gmail.com>
2023-01-04 22:59:43 -0800

    add tab view and grid view

--- lib/Screens/albums.dart
+++ lib/Screens/albums.dart
@@ -12,8 +12,11 @@ class AlbummsScreen extends StatefulWidget {
   State<AlbummsScreen> createState() => _AlbummsScreenState();
 }
 
-class _AlbummsScreenState extends State<AlbummsScreen> {
+class _AlbummsScreenState extends State<AlbummsScreen>
+    with TickerProviderStateMixin {
   List<Album> alist = [];
+  double? width;
+  double? height;
   @override
   void initState() {
     // TODO: implement initState
@@ -23,25 +26,76 @@ class _AlbummsScreenState extends State<AlbummsScreen> {
 
   @override
   Widget build(BuildContext context) {
+    height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
+    TabController _tabController = TabController(length: 3, vsync: this);
     return Scaffold(
       appBar: AppBar(
         title: Text("Albums"),
         backgroundColor: primaryColor,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 15, left: 15),
-          child: Wrap(
-            spacing: 14,
-            runSpacing: 15,
-            children: [
-              ...alist.map(
-                (e) => CustomAlbum(e.title, e.image, 85, 100),
-              ),
-            ],
+      body: Column(
+        children: [
+          Container(
+            height: height! * 0.08,
+            child: TabBar(
+                indicatorColor: primaryColor,
+                controller: _tabController,
+                labelColor: Colors.black,
+                tabs: [
+                  Text(
+                    "People",
+                  ),
+                  Text(
+                    "Event",
+                  ),
+                  Text(
+                    "Location",
+                  )
+                ]),
           ),
-        ),
+          Container(
+            width: width,
+            height: height! * 0.71,
+            child: TabBarView(
+                controller: _tabController,
+                children: [peoplealbums(), peoplealbums(), peoplealbums()]),
+          ),
+        ],
       ),
     );
+  }
+
+  Widget peoplealbums() {
+    return Container(
+      padding: EdgeInsets.only(top: 15),
+      child: GridView.count(
+        crossAxisCount: 3,
+        children: [
+          ...alist.map(
+            (e) => CustomAlbum(e.title, e.image, 85, 100),
+          ),
+          ...alist.map(
+            (e) => CustomAlbum(e.title, e.image, 85, 100),
+          ),
+        ],
+      ),
+    );
+
+    // return Padding(
+    //   padding: const EdgeInsets.only(top: 15, left: 15),
+    //   child: Wrap(
+    //     spacing: 14,
+    //     runSpacing: 15,
+    //     children: [
+    //       ...alist.map(
+    //         (e) => CustomAlbum(e.title, e.image, 85, 100),
+    //       ),
+    //       ...alist.map(
+    //         (e) => CustomAlbum(e.title, e.image, 85, 100),
+    //       ),
+    //     ],
+    //   ),
+    // );
   }
 }
